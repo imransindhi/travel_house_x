2536541ae06485cae3347d479e5be9b086079bf1
Imran <imransindhi162@gmail.com>
2020-03-09 17:25:01 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:travel_house_x/Screens/SplashScreen.dart';
+
+void main() => MainApp();
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
