d086c23b8735ecebc20ab3483673cac5100a53e5
Imran <imransindhi162@gmail.com>
2020-03-21 04:19:57 -0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_house_x/screens/LoginScreen.dart';
+import 'package:travel_house_x/screens/login_screen.dart';
 
 void main() => runApp(new MainApp());
 
