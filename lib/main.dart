6cb82d9683f1e24c56d9244135567a22f3cbfd59
JairajJangle <jairaj.jangle@gmail.com>
2020-04-01 11:11:02 +0530

    Added HomeScreen route name

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:travel_house_x/screens/login_screen.dart';
-import 'package:travel_house_x/screens/splash_screen.dart';
+import 'package:travel_house_x/screens/HomeScreen.dart';
+import 'package:travel_house_x/screens/LoginScreen.dart';
+import 'package:travel_house_x/screens/SplashScreen.dart';
 
 void main() => runApp(new MaterialApp(
-  home: new SplashScreen(),
-  routes: <String, WidgetBuilder>{
-    '/LoginScreen': (BuildContext context) => new LoginScreen()
-  },
-));
+      home: new SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        '/LoginScreen': (BuildContext context) => new LoginScreen(),
+        '/HomeScreen': (BuildContext context) => new HomeScreen()
+      },
+    ));
 
 class MainApp extends StatelessWidget {
   @override
