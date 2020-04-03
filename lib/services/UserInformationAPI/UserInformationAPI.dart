93817a31804f4d0790a43403ba15c4b315f82812
JairajJangle <jairaj.jangle@gmail.com>
2020-04-03 22:32:16 +0530

    Auto indent

--- lib/services/UserInformationAPI/UserInformationAPI.dart
+++ lib/services/UserInformationAPI/UserInformationAPI.dart
@@ -1,16 +1,16 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:travel_house_x/utils/LogUtils.dart';
 
-class UserInformationAPI{
-  Future<void> getUserInformation() async
-  {
+class UserInformationAPI {
+  Future<void> getUserInformation() async {
     String URL = "http://apidev.travelhouse.world/api/v1/user/view";
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    var response = await http.post(Uri.encodeFull(URL), headers: {"Authorization": sharedPreferences.getString('token')});
+    var response = await http.post(Uri.encodeFull(URL),
+        headers: {"Authorization": sharedPreferences.getString('token')});
     var status = json.decode(response.body.toString());
     sharedPreferences.setString("user_id", status['user_id']);
-    print(status['other'
-        '']);
+    LogUtils.d(status['other' '']);
   }
 }
