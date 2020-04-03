2916075b3f97ea8c9057d98a67c474f9fd00086b
JairajJangle <jairaj.jangle@gmail.com>
2020-04-03 22:33:47 +0530

    1. Implemented Packages API Interface 2. Auto Indent

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,55 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:travel_house_x/services/PackagesAPI.dart';
-import 'package:travel_house_x/services/UserInformationAPI.dart';
+import 'package:travel_house_x/dataClasses/PackageInfo.dart';
+import 'package:travel_house_x/services/PackagesAPI/PackagesAPI.dart';
+import 'package:travel_house_x/services/PackagesAPI/PackagesAPIInterface.dart';
+import 'package:travel_house_x/services/UserInformationAPI/UserInformationAPI.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    implements PackagesAPIInterface {
   PackagesAPI packagesAPI = new PackagesAPI();
+  List<PackageInfo> packagesInfoList = List();
+
   UserInformationAPI userInformationAPI = new UserInformationAPI();
+
   @override
   Widget build(BuildContext context) {
-    packagesAPI.getPackageInformation();
+    packagesAPI.getPackageInformation(this);
     return MaterialApp(
       home: DefaultTabController(
-          length: choices.length,
-          child: Scaffold(
+        length: choices.length,
+        child: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.black87,
-          title: const Text('Travel House'),
-      bottom: TabBar(
-
-        isScrollable: true,
-        tabs: choices.map<Widget>((Choice choice) {
-          return Tab(
-            text: choice.title,
-            icon: Icon(choice.icon),
-          );
-        }).toList(),
+            title: const Text('Travel House'),
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: choices.map<Widget>((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ChoicePage(
+                  choice: choice,
+                ),
+              );
+            }).toList(),
+          ),
+        ),
       ),
-    ),
-    body: TabBarView(
-    children: choices.map((Choice choice) {
-    return Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: ChoicePage(
-    choice: choice,
-    ),
     );
-    }).toList(),
-    ),
-    ),
-      ),
-    );
+  }
 
+  @override
+  void onFailurePackages(String message) {
+    // TODO: Display Package Get Error here
+  }
+
+  @override
+  void onSuccessPackages(List<PackageInfo> packagesInfoList) {
+    this.packagesInfoList = packagesInfoList;
   }
 }
 
 class Choice {
   final String title;
   final IconData icon;
+
   const Choice({this.title, this.icon});
 }
 
