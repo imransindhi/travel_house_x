79785abd558d3ddbfb6768aacad078f6d91b44c9
JairajJangle <jairaj.jangle@gmail.com>
2020-04-03 22:32:42 +0530

    Added Packages API Interface

---
+++ lib/services/PackagesAPI/PackagesAPIInterface.dart
@@ -0,0 +1,7 @@
+import 'package:travel_house_x/dataClasses/PackageInfo.dart';
+
+class PackagesAPIInterface {
+  void onSuccessPackages(List<PackageInfo> packagesInfoList) {}
+
+  void onFailurePackages(String message) {}
+}
